2175848e462c2d13938091cb1b582c7ced47b37b
USUARIO <USUARIO@LAB1_PC_12>
2025-11-19 09:16:49 -0600

    Agregar archivos de proyecto.

---
+++ PruebaRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
